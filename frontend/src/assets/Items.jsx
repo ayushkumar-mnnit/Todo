e8c1b4b93e71f1727dd6009f79b8fe661bf854b8
ayushkumar-mnnit <ayushk.2125@gmail.com>
2024-08-17 15:09:37 +0530

    updated

--- frontend/src/assets/Items.jsx
+++ frontend/src/assets/Items.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import "./item.css";
 
+import img1 from "../assets/wrong-icon.png"
+
 const Items = () => {
   const getItemsFromLocalStorage = () => {
-    const result = localStorage.getItem("myList"); 
-    return result ? JSON.parse(result) : []; // Always return an array
+    const result = localStorage.getItem("myList");
+    return result ? JSON.parse(result) : [];
   };
 
   const [newItem, setNewItem] = useState("");
-  const [items, setItems] = useState(getItemsFromLocalStorage()); // Correctly initializing state
+  const [items, setItems] = useState(getItemsFromLocalStorage());
 
   const handleChange = (e) => {
     setNewItem(e.target.value);
@@ -53,13 +55,16 @@ const Items = () => {
         <ul className="todo-list">
           {items.length > 0 && items.map((item, index) => (
             <li key={index} className="todo-item">
-              {item}
-              <button onClick={() => deleteItem(index)} className="delete-btn">
-                Delete
-              </button>
+              <div className="todo-card">
+                {item}
+                <button onClick={() => deleteItem(index)} className="delete-btn">
+                <img src={img1} width={20} height={20} alt="remove all" />
+                </button>
+              </div>
             </li>
           ))}
         </ul>
+        {items.length > 0 && <button onClick={removeAll} className="remove-all-btn">Remove all</button>}
       </div>
     </>
   );
